42854ae68faf76c8d05b0e26a1b84028324e139a
doulu <wb-dl449432@alibaba-inc.com>
2019-09-18 18:08:28 +0800

    province data

--- src/pages/dnd/index.js
+++ src/pages/dnd/index.js
@@ -1,10 +1,90 @@
 import React, { Component } from 'react';
+import propTypes from 'prop-types';
+import _ from 'lodash';
+import './style.css';
+import { message, TreeSelect } from 'antd';
+import { ThreeLevelProvinceData, PROVINCE, CITY, COUNTY } from '../../utils'
+console.log(_.flatMapDeep([1, [2, 3], [4, [5]], 6, [7, 8]]));
+
 export default class DragDemo extends Component {
+  static propTypes = {
+    value: propTypes.string,
+    dataSource: propTypes.array,
+  };
+  static defaultProps = {
+    value: '',
+    dataSource: [],
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      weatherinfo: null,
+      pid: null,
+      cid: null,
+    };
+  }
+  componentDidMount() {
+    const cityCode = '101210103';
+    const apiUrl = `${window.location.origin}/data/cityinfo/${cityCode}.html`;
+    fetch(apiUrl)
+      .then(res => res.json())
+      .then(data => { this.setState({ weatherinfo: data.weatherinfo }) })
+      .catch(error => { message.error(error) })
+
+    // 省份、城市数据
+    fetch('https://os.alipayobjects.com/rmsportal/ODDwqcDFTLAguOvWEolX.json')
+      .then(response => response.json())
+      .then((data) => { this.setState({ data }) })
+      .catch(e => console.log(e));
+  }
   render() {
+    const { weatherinfo, data, pid, cid } = this.state;
     return (
       <div>
-        react lib drag and drop
-        react lib drag and drop
+        {weatherinfo && <div>
+          <p>城市：{weatherinfo.city}</p>
+          <p>天气：{weatherinfo.weather}</p>
+          <p>
+            <span className={`icon ${weatherinfo.img1.split('0')[0]} fl`}></span>
+            <span className={`icon ${weatherinfo.img2.split('0')[0]} fl`}></span>
+          </p>
+          <p>时间：{weatherinfo.ptime}</p>
+          <p>最低温度：{weatherinfo.temp1}</p>
+          <p>最高温度：{weatherinfo.temp2}</p>
+        </div>}
+        <div>
+          fetch data:
+         <TreeSelect
+            showSearch
+            style={{ width: 160 }}
+            placeholder={'选择城市'}
+            treeData={data}
+          />
+        </div>
+        <div>
+          json data:
+         <TreeSelect
+            showSearch
+            style={{ width: 160 }}
+            placeholder={'选择城市'}
+            treeData={ThreeLevelProvinceData}
+          />
+        </div>
+        <div>
+          <span>
+            省：
+            <TreeSelect onChange={(value) => { this.setState({ pid: value }) }} style={{ width: 160 }} placeholder={'选择省：'} treeData={PROVINCE} />
+          </span>
+          <span>
+            市：
+            <TreeSelect onChange={(value) => { this.setState({ cid: value }) }} style={{ width: 160 }} placeholder={'选择市：'} treeData={pid && CITY(pid)} />
+          </span>
+          <span>
+            地区：
+            <TreeSelect style={{ width: 160 }} placeholder={'选择地区：'} treeData={cid && COUNTY(cid)} />
+          </span>
+        </div>
       </div>
     );
   }
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,7 @@
+import province from '../data/province.json';
+import city from '../data/city.json';
+import county from '../data/county.json';
+
 // 无副作用，数组操作
 const arrayInsert = (arr, index, newItem) => [ // 数组插入解构形式
   ...arr.slice(0, index),
@@ -12,8 +16,29 @@ const arrayRemove = (arr, index) => (arr.length === 1 ? [] : [ // 数组删除
 
 const copy = obj => JSON.parse(JSON.stringify(obj));
 
+// 三级省市县数据
+const ThreeLevelProvinceData = province.map(pro => {
+  const proData = { title: pro.name, value: pro.id };
+  proData.children = !city[pro.id] ? [] : city[pro.id].map(pCity => {
+    const cityData = { title: pCity.name, value: pCity.id };
+    cityData.children = !county[pCity.id] ? [] : county[pCity.id].map(cc => {
+      return { title: cc.name, value: cc.id };
+    })
+    return cityData;
+  });
+  return proData;
+});
+
+const PROVINCE = province.map(pro => ({ title: pro.name, value: pro.id }));
+const CITY = (pid) => city[pid].map(pro => ({ title: pro.name, value: pro.id }));
+const COUNTY = (cid) => county[cid].map(pro => ({ title: pro.name, value: pro.id }));
+
 export {
   arrayInsert,
   arrayRemove,
   copy,
+  PROVINCE,
+  CITY,
+  COUNTY,
+  ThreeLevelProvinceData,
 };
