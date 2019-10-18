c940fae9459c9c0c77d15be116a4246978c654b9
doulu <doulu@douludeMacBook-Pro.local>
2019-10-18 20:36:55 +0800

    Updates

---
+++ precache-manifest.11a69f2609e3ca3a9f785e967e533fc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b9814b7e70575233265ed85c4cfdbca",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2b177059d5048c01351a",
+    "url": "./static/css/2.f1cd5980.chunk.css"
+  },
+  {
+    "revision": "a645f82c65abd65ca27d",
+    "url": "./static/css/main.6835090d.chunk.css"
+  },
+  {
+    "revision": "2b177059d5048c01351a",
+    "url": "./static/js/2.af9f7ca1.chunk.js"
+  },
+  {
+    "revision": "a645f82c65abd65ca27d",
+    "url": "./static/js/main.b4d54706.chunk.js"
+  },
+  {
+    "revision": "2bccefb49f8bff3d201a",
+    "url": "./static/js/runtime~main.eccdafba.js"
+  }
+]);
